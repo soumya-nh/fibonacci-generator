407e5adc8ee239b88646c4880c1d9e9b736e7939
soumya-nh <soumyanh1212@gmail.com>
2024-09-01 21:01:37 +0530

    "C:\Users\soumy\codealpha(task 1).py"

---
+++ codealpha(task 1).py
@@ -0,0 +1,15 @@
+#fibonacci_series_generator
+def fib():
+    a=1
+    b=1
+    while True:
+        f =a+b
+        a=b
+        b=f
+        yield f
+
+fib_gen = fib()
+
+for i in range(10):
+
+    print(next(fib_gen))
